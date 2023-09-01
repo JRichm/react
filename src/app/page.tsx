369b2619566db0176d98b65835deb432298d9bfa
James Richmond <jblackenmusic@gmail.com>
2023-09-01 13:26:17 -0600

    added styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from '@/components/button';
 export default function HomePage() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
+      <h1>Hello World</h1>
       <Button />
     </>
   )
---
+++ src/app/pages/index.js
@@ -0,0 +1,11 @@
+import Button from '@components/button';
+import '@styles/styles.css';
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Hello from react!</h1>
+            <Button />
+        </>
+    )
+}
