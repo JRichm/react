9a52ea298fd5ba9b991d33ebb49c50d3e429fd1d
James Richmond <jblackenmusic@gmail.com>
2023-09-08 19:32:56 -0600

    added new note functionality back

--- src/app/components/dayDetails.tsx
+++ src/app/components/dayDetails.tsx
@@ -1,13 +1,18 @@
-
+import React, { useState } from 'react';
 let fakeNotes = ['note 1', 'note 2']
 
 export default function DayDetails({selectedDate, dateNotes}) {
+
+    const [addingNote, setAddingnote] = useState(false);
 
     let noteElements = []
 
     dateNotes.forEach(note => {
         noteElements.push(
-            <li>{ note }</li>
+            <>
+                <li>{ note }</li>
+                <hr />
+            </>
         )
     })
     
@@ -20,10 +25,29 @@ export default function DayDetails({selectedDate, dateNotes}) {
                 <ul>
                     {noteElements}
                 </ul>
-                <div className="w-full flex justify-center">
-                    <img className="h-8 text-center opacity-20 m-6" src="https://cdn3.iconfinder.com/data/icons/eightyshades/512/14_Add-512.png"></img>
-                </div>
+                <NewNote addingNote={addingNote} setAddingnote={setAddingnote}/>
             </div>
         </>
     )
+}
+
+function NewNote({addingNote, setAddingnote}) {
+    if (addingNote) {
+        return (
+            <>
+                <form className='flex flex-col my-5 p-2 border border-solid border-black'>
+                    <input type='text' placeholder='note title' className='border-none outline-none m-2'></input>
+                    <hr />
+                    <textarea placeholder='new note' className='border-none outline-none m-2'></textarea>
+                    <input type="submit" value={"add"} className='hover:cursor-pointer bg-gray-200 px-3 m-2'></input>
+                </form>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <img className="h-8 text-center opacity-20 m-6" src="https://cdn3.iconfinder.com/data/icons/eightyshades/512/14_Add-512.png" onClick={e => setAddingnote(true)}></img>
+            </>
+        )
+    }
 }
