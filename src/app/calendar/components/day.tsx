0235333c0a87f9b6e8c695efd2c279a437d1c92a
James Richmond <jblackenmusic@gmail.com>
2023-09-29 15:04:14 -0600

    minor tweaks

--- src/app/calendar/components/day.tsx
+++ src/app/calendar/components/day.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 export default function Day({date, notes, selectedDate}) {
-    let style
+    let selectedStyle
+    let currentDateStyle
     console.log(date.getDate(), notes)
 
     function noteDotElements() {
@@ -15,18 +16,23 @@ export default function Day({date, notes, selectedDate}) {
         return noteDots
     }
 
-    if (date == selectedDate) {
-        style = 'border border-black border-1'
-    } else {
-        style = ''
+    console.log(selectedDate)
+
+    if (date.getMonth() == selectedDate.getMonth() && date.getDate() == selectedDate.getDate()) {
+        selectedStyle += 'border border-gray-200 border-1 shadow-sm shadow-black/50'
+    }
+
+    const currentDate = new Date()
+    if (date.getMonth() == currentDate.getMonth() && date.getDate() == currentDate.getDate()) {
+        currentDateStyle = 'rounded-full bg-blue-500 text-center text-white'
     }
 
     return (
         <>
-            <div className={`${style} hover:outline hover:outline-1 hover:outline-gray-200 m-1 p-1 h-16`}>
+            <div className={`${selectedStyle} hover:outline hover:outline-1 hover:outline-gray-200 m-1 p-1 h-16`}>
                 <div>
                     <span>
-                        <p className='p-0 m-0'>{date.getDate()}</p>
+                        <p className={`w-6 h-6 text-center  ${currentDateStyle}`}>{date.getDate()}</p>
                     </span>
                     <div className='flex flex-row'>
                         {noteDotElements()}
